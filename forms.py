3c2788b62c75ff139e66631208aac1ba58595c9c
vovans32 <kpvcha4@yandex.ru>
2020-05-01 14:18:34 +0500

    Merge with all branches

---
+++ forms.py
@@ -0,0 +1,39 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms import IntegerField, BooleanField, FileField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    """ форма регистрации """
+
+    email = EmailField('E-mail:', validators=[DataRequired()])
+    password = PasswordField('Пароль:', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль:', validators=[DataRequired()])
+    surname = StringField('Фамилия:', validators=[DataRequired()])
+    name = StringField('Имя:', validators=[DataRequired()])
+    age = IntegerField('Возраст:', validators=[DataRequired()])
+    about = StringField('О себе:')
+    education = StringField('Образование:', validators=[DataRequired()])
+    speciality = StringField('Род деятельности:', validators=[DataRequired()])
+    agree = BooleanField('Согласен на обработку персональных данных:', validators=[DataRequired()])
+    avatar = FileField('Загрузите фото профиля')
+    submit = SubmitField('Зарегистрироваться')
+
+
+class LoginForm(FlaskForm):
+    """ форма входа на сайт """
+
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class PostForm(FlaskForm):
+    """ форма создания поста """
+
+    title = StringField('Заголовок', validators=[DataRequired()])
+    content = TextAreaField("Содержание")
+    submit = SubmitField('Запостить')
--- main.py
+++ main.py
@@ -1,18 +1,197 @@
-from flask import Flask, render_template, url_for
-from pars_html import pars_html
+from flask import Flask, render_template, url_for, redirect
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from data import db_session
+from data.users import User
+from data.regions import Region
+from data.posts import Post
+from forms import RegisterForm, LoginForm, PostForm
+from werkzeug.utils import secure_filename
+import os
+from PIL import Image
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-@app.route('/yandex')
-def return_yandex_page():
-    return render_template('base.html', css=url_for('static', filename='css/style.css'))
+def main():
+    db_session.global_init("db/database.sqlite")
+    app.run(port=8080, host='127.0.0.1')
 
 
-@app.route('/1')
-def return_yandex1_page():
-    return render_template('yandex1.html', css=url_for('static', filename='css/style.css'))
+@app.route('/')
+def main_page():
+    """ обработчик главной страницы """
+
+    return render_template('home.html', title='Коронавирус', css=url_for('static', filename='css/home_style.css'))
+
+
+@app.route("/blog")
+def index():
+    """ Обработчик страницы с блогом """
+
+    session = db_session.create_session()
+    posts = session.query(Post).all()
+    posts = sorted(posts, key=lambda x: x.created_date, reverse=True)
+    return render_template("blog.html", posts=posts, title='Блог',
+                           css=url_for('static', filename='css/blog_style.css'))
+
+
+@app.route('/post', methods=['GET', 'POST'])
+@login_required
+def add_post():
+    """ Обработчик страницы создания поста """
+
+    form = PostForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        post = Post()
+        post.title = form.title.data
+        post.content = form.content.data
+        current_user.posts.append(post)
+        session.merge(current_user)
+        session.commit()
+        return redirect('/blog')
+    return render_template('post.html', title='Новый пост',
+                           form=form, css=url_for('static', filename='css/post_style.css'))
+
+
+@app.route('/regions')
+def regions():
+    """ Обработчик страницы со списком регионов """
+
+    session = db_session.create_session()
+    regions = session.query(Region).all()
+    return render_template('regions.html', title='Регионы', regions=regions,
+                           css=url_for('static', filename='css/regions_style.css'))
+
+
+@app.route('/regions/<region_id>')
+def region(region_id):
+    """ Обработчик страницы региона """
+
+    session = db_session.create_session()
+    region = session.query(Region).filter(Region.id == region_id).first()
+    return render_template('region.html', title=region.name, region=region,
+                           css=url_for('static', filename='css/region_style.css'))
+
+
+@app.route('/join', methods=['GET', 'POST'])
+def join():
+    """ обработчик регистрации пользователя """
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('join.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают",
+                                   css=url_for('static', filename='css/join_style.css'))
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('join.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть",
+                                   css=url_for('static', filename='css/join_style.css'))
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            email=form.email.data,
+            about=form.about.data,
+            education=form.education.data,
+            speciality=form.speciality.data
+        )
+
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+
+        # добавим аватар, если пользователь загрузил его
+
+        if bool(form.avatar.data):
+            f = form.avatar.data
+            type = f.filename.split('.')[-1]
+            filename = f'user_avatar_{user.id}.{type}'
+            f.save(os.path.join(
+                'static', 'avatars', filename
+            ))
+
+            #  сделаем аватар квадратным
+
+            im = Image.open(os.path.join(
+                'static', 'avatars', filename
+            ))
+            pixels = im.load()  # список с пикселями
+            x, y = im.size
+            if x > y:
+                size = y
+            else:
+                size = x
+            avatar = Image.new('RGB', (size, size), (0, 0, 0))
+            av_pixels = avatar.load()
+
+            # фото обрезается по центру
+
+            dx = (x - size) // 2
+            dy = (y - size) // 2
+
+            for i in range(size):
+                for j in range(size):
+                    r, g, b = pixels[i + dx, j + dy]
+                    av_pixels[i, j] = r, g, b
+            avatar.save(os.path.join(
+                'static', 'avatars', filename
+            ))
+
+            session = db_session.create_session()
+            user = session.query(User).filter(User.id == user.id).first()
+            user.avatar = os.path.join(
+                'static', 'avatars', filename
+            )
+            session.commit()
+
+        return redirect('/login')
+    return render_template('join.html', title='Регистрация', form=form,
+                           css=url_for('static', filename='css/join_style.css'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """ обратчик страницы с авторизацией """
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form,
+                               css=url_for('static', filename='css/login_style.css'))
+    return render_template('login.html', title='Авторизация', form=form,
+                           css=url_for('static', filename='css/login_style.css'))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """ обработчик входа пользователя """
+
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    """ обработчик выхода пользователя """
+
+    logout_user()
+    return redirect("/")
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()

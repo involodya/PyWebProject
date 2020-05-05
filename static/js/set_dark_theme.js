c632db7f52d350e7ec8d2d1c60245e682ae08444
victormorozov1 <vmn3w@yandex.ru>
2020-05-05 11:13:42 +0500

    ok

--- static/js/set_dark_theme.js
+++ static/js/set_dark_theme.js
@@ -1,5 +1,6 @@
 html = document.querySelector('html');
 a = [['--background-color', '#002137'],
+    ['--nav-background-color', '#000305'],
     ['--text-color', '#999999'],
 
     ['--quiz-a-text-color', '#999999'],
@@ -8,7 +9,9 @@ a = [['--background-color', '#002137'],
     ['--quiz-a-hover-color', '#ffba42'],
     ['--nav-color', '#242424'],
     ['--blog-content-unit-color', '#222222'],
-    ['--second-background-color', '#888888']];
+    ['--second-background-color', '#888888'],
+    ['--second-text-color', 'white'],
+    ['--input-border-color', '#999999']];
 for (let i of a) {
     html.style.setProperty(i[0], i[1]);
 }
--- static/js/set_white_theme.js
+++ static/js/set_white_theme.js
@@ -1,5 +1,5 @@
 html = document.querySelector('html');
-a = [['--background-color', 'white'],
+a = [['--background-color', '#999999'],
     ['--text-color', '#222222'],
 
     ['--quiz-a-text-color', '#999999'],
@@ -8,7 +8,9 @@ a = [['--background-color', 'white'],
     ['--quiz-a-hover-color', '#7B68EE'],
     ['--nav-color', '#72848f'],
     ['--content-unit-color', '#999999'],
-    ['--second-background-color', 'white']];
+    ['--second-background-color', 'white'],
+    ['--second-text-color', '#222222'],
+    ['--input-border-color', '#888888']];
 for (let i of a) {
     html.style.setProperty(i[0], i[1]);
 }
